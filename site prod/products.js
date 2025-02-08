eca9c4a30cdb545b8b0bcb55565ee484a2a5642e
Araha1911 <slavonsavelev91@gmail.com>
2025-02-08 02:32:02 +0200

    Add files via upload

---
+++ site prod/products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        id: 1,
+        name: "Товар 1",
+        description: "Описание товара 1",
+        price: "1000 грн",
+        image: "product1.jpg.src",
+    },
+    {
+        id: 2,
+        name: "Товар 2",
+        description: "Описание товара 2",
+        price: "1500 грн",
+        image: "product2.jpg.src",
+    },
+];
+
+// Сгенерируйте товары динамически
